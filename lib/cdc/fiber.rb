10a31b950e6dfe0ce1c673b03a2cba3f96ee8659
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2026-05-31 20:08:21 +0800

    chore: initialize cdc-fiber gem structure

---
+++ lib/cdc/fiber.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "fiber/version"
+
+module Cdc
+  module Fiber
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/cdc/fiber_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cdc::Fiber do
+  it "has a version number" do
+    expect(Cdc::Fiber::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
